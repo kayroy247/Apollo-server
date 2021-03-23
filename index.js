c631389e701e581d74f1db61d5c8a88f2c9b87d6
kayroy247 <okunladekayode@gmail.com>
2021-03-23 15:22:19 +0100

    feat:add nodemon

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const typeDefs = gql`
   type Book {
     title: String
     author: String
+    ISBN: Int
+  }
+
+  type Users {
+    firstname: String
+    lastname: String
+    bio: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -17,26 +24,45 @@ const typeDefs = gql`
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    users: [Users]
   }
 `;
 
 const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+    ISBN: 128928
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+    ISBN: 1928388
+  },
 ];
+
+const users = [
+  {
+    firstname: 'Thomas',
+    lastname: 'Kate',
+    bio: 'Chelsea fan'
+  },
+  {
+    firstname: 'Henry',
+    lastname: 'John',
+    bio: 'Arsenal Fan'
+  },
+
+];
+
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-    Query: {
-        books: () => books,
-    },
+  Query: {
+    books: () => books,
+    users: () => users
+  },
 };
 
 // The ApolloServer constructor requires two parameters: your schema
@@ -45,5 +71,5 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}0`);
 });
